16f384d135cb9e3f6c1d33dd2ba0923a044de70d
criverao <c.riverao@uniandes.edu.co>
2022-10-29 18:04:35 -0500

    Crear navegación principal de la aplicación

---
+++ app/src/main/java/com/example/ingsoftappmobiles/viewmodels/ArtistsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ingsoftappmobiles.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ArtistsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is artists Fragment"
+    }
+    val text: LiveData<String> = _text
+}
