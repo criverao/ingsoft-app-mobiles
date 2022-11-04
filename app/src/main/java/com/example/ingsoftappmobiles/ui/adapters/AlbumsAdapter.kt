7ac68e0aab07257c2fb2579a19afa480537e3a63
criverao <c.riverao@uniandes.edu.co>
2022-11-03 21:58:53 -0500

    Crear los componentes restantes de la arquitectura, repositorio y adaptadores

---
+++ app/src/main/java/com/example/ingsoftappmobiles/ui/adapters/AlbumsAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.ingsoftappmobiles.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ingsoftappmobiles.R
+import com.example.ingsoftappmobiles.databinding.AlbumItemBinding
+import com.example.ingsoftappmobiles.models.Album
+
+class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumsViewHolder>(){
+
+    var albums :List<Album> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumsViewHolder {
+        val withDataBinding: AlbumItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AlbumsViewHolder.LAYOUT,
+            parent,
+            false)
+        return AlbumsViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: AlbumsViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.album = albums[position]
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return albums.size
+    }
+
+    class AlbumsViewHolder(val viewDataBinding: AlbumItemBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.album_item
+        }
+    }
+}
