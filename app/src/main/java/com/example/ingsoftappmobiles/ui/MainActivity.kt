03e39c35269a634ab8a2eb4defb395ce43743216
criverao <c.riverao@uniandes.edu.co>
2022-11-30 22:35:37 -0500

    Mover el botón de creación de álbum al fragmento del listado

--- app/src/main/java/com/example/ingsoftappmobiles/ui/MainActivity.kt
+++ app/src/main/java/com/example/ingsoftappmobiles/ui/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.ingsoftappmobiles.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -20,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private var menu2:Menu? = null
+    //private lateinit var createAlbumFab: ExtendedFloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,66 +27,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
-        // Get the navigation host fragment from this Activity
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-        // Instantiate the navController using the NavHostFragment
-        navController = navHostFragment.navController
-        // Make sure actions in the ActionBar get propagated to the NavController
-        Log.d("act", navController.toString())
-        setSupportActionBar(findViewById(R.id.my_toolbar))
-        //setupActionBarWithNavController(navController)
 
-        //navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = navHostFragment.navController
+
+        Log.d("act", navController.toString())
+
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_albums, R.id.navigation_artists, R.id.navigation_collectors
             )
         )
-        //setSupportActionBar(findViewById(R.id.my_toolbar))
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        if (menu != null) {
-            menu2 = menu
-        }
-        menuInflater.inflate(R.menu.action_bar_menu, menu)
-        return true
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        val parts:List<String> = getSupportFragmentManager().fragments[0].childFragmentManager.fragments[0].javaClass.name.split(".")
-        menu?.findItem(R.id.create_albums)?.setVisible(parts.last().equals("AlbumFragment"))
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.create_albums -> {
-                // User chose the "Settings" item, show the app settings UI...
-                val navController:NavController = findNavController(R.id.nav_host_fragment_activity_main)
-                navController.navigate(R.id.action_navigation_albums_to_navigation_create_album)
-                return true
-            }
-
-            else -> {
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item)
-            }
-
-        }
+        /*createAlbumFab = findViewById(R.id.create_album_fab)
+        binding.createAlbumFab.setOnClickListener {
+            navController.navigate(R.id.action_navigation_albums_to_navigation_create_album)
+            createAlbumFab.visibility = View.GONE
+        }*/
     }
 
     override fun onSupportNavigateUp(): Boolean {
+        //createAlbumFab.visibility = View.VISIBLE
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
