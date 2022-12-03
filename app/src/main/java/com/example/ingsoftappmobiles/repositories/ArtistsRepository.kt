aed664e670bffb036c57681a681539e40df3737c
criverao <c.riverao@uniandes.edu.co>
2022-12-03 16:14:12 -0500

    Implementar manejador de caché para álbumes, artistas y coleccionanistas

--- app/src/main/java/com/example/ingsoftappmobiles/repositories/ArtistsRepository.kt
+++ app/src/main/java/com/example/ingsoftappmobiles/repositories/ArtistsRepository.kt
@@ -1,17 +1,26 @@
 package com.example.ingsoftappmobiles.repositories
 
 import android.app.Application
-import com.android.volley.VolleyError
+import android.util.Log
 import com.example.ingsoftappmobiles.models.Artist
+import com.example.ingsoftappmobiles.network.CacheManager
 import com.example.ingsoftappmobiles.network.ArtistServiceAdapter
 
 class ArtistsRepository (val application: Application) {
     suspend fun refreshData() : List<Artist> {
-        var artists: List<Artist> = ArtistServiceAdapter.getInstance(application).getMusiciansOnArtist()
 
-        val bands = ArtistServiceAdapter.getInstance(application).getBandsOnArtist()
-
-        return artists + bands
+        val potentialResp = CacheManager.getInstance(application.applicationContext).getArtists()
+        return if(potentialResp.isEmpty()){
+            Log.d("Cache decision", "get from network")
+            val musicians: List<Artist> = ArtistServiceAdapter.getInstance(application).getMusiciansOnArtist()
+            val bands = ArtistServiceAdapter.getInstance(application).getBandsOnArtist()
+            val artists = musicians + bands
+            CacheManager.getInstance(application.applicationContext).addArtists(artists)
+            artists
+        } else{
+            Log.d("Cache decision", "return ${potentialResp.size} elements from cache")
+            potentialResp
+        }
 
     }
 }
