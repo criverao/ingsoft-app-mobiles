72e74ca8a7b80f60fd62ac72ac0e90b2075e774e
“ingecar” <j.cardonao@uniandes.edu.co>
2022-12-04 19:12:45 -0500

    Pruebas espresso hu04 consultar informacion detallada de artista

--- app/src/androidTest/java/com/example/ingsoftappmobiles/ui/DetalleArtistTest.kt
+++ app/src/androidTest/java/com/example/ingsoftappmobiles/ui/DetalleArtistTest.kt
@@ -3,9 +3,11 @@ package com.example.ingsoftappmobiles.ui
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -13,6 +15,7 @@ import androidx.test.filters.LargeTest
 import com.example.ingsoftappmobiles.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
@@ -21,47 +24,72 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class AlbumesTest {
+class DetalleArtistTest {
 
     @Rule
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun albumesTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        Thread.sleep(500)
-
+    fun detalleArtistTest() {
         val bottomNavigationItemView = onView(
             allOf(
-                withId(R.id.albumsFragment), withContentDescription("Álbumes"),
+                withId(R.id.navigation_artists), withContentDescription("Artistas"),
                 childAtPosition(
                     childAtPosition(
                         withId(R.id.nav_view),
                         0
                     ),
-                    0
+                    1
                 ),
                 isDisplayed()
             )
         )
         bottomNavigationItemView.perform(click())
 
-        val textView = onView(
+        val recyclerView = onView(
             allOf(
-                withText("Álbumes"),
-                withParent(
+                withId(R.id.artistsRv),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val appCompatImageButton = onView(
+            allOf(
+                withContentDescription("Navigate up"),
+                childAtPosition(
                     allOf(
                         withId(androidx.constraintlayout.widget.R.id.action_bar),
-                        withParent(withId(androidx.constraintlayout.widget.R.id.action_bar_container))
-                    )
+                        childAtPosition(
+                            withId(androidx.constraintlayout.widget.R.id.action_bar_container),
+                            0
+                        )
+                    ),
+                    2
                 ),
                 isDisplayed()
             )
         )
-        textView.check(matches(withText("Álbumes")))
+        appCompatImageButton.perform(click())
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.navigation_artists), withContentDescription("Artistas"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
     }
 
     private fun childAtPosition(
